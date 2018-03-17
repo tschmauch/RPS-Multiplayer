4113f3b3ad2fe280879cbf6680f0fb76c1ec5295
Tyler Schmauch <tschmauch@hotmail.com>
2018-03-17 12:51:17 -0600

    player sign in

--- assets/app.js
+++ assets/app.js
@@ -13,6 +13,31 @@
 
 	var wins1 = 0;
 	var wins2 = 0;
-	var player1;
-	var player2;
-	var 
+	var player1 = '';
+	var player1bool = false;
+	var player2 = '';
+	var player2bool = false;
+	
+
+	// Players sign-in
+	$('#submit').on('click', function() {
+		if (player1bool == false) {
+			$('#player-1-name').text($('#username').val());
+			database.ref().set({
+				user1: $('#username').val()
+			});
+			$('#username').val('');
+			player1bool = true;
+		}
+		else if (player1bool == true && player2bool == false){
+			$('#player-2-name').text($('#username').val());
+			database.ref().set({
+				user2: $('#username').val()
+			});
+			$('#username').val('');
+			player2bool = true;
+		}
+		else {
+			alert('Lobby is full.');
+		}
+	})
